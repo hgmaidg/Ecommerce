551aad1a5bba74ab292daaf44ff092c8fea06efc
Hoàng Mai <hgmaidg@gmail.com>
2023-10-26 13:28:29 +0700

    Login/Logout/Register

---
+++ docs.js
@@ -0,0 +1,16 @@
+//CONFIG
+//Tạo DB connection
+//Tạo các funtion có thể tái sử dụng
+
+//CONTROLLER
+//Kết nối model vào controller
+//Tạo các chức năng như create, read, update, delete
+
+//MIDDELWARE
+//create oauth middleware error middleware
+
+//MODELS
+//write out schemas
+
+//ROUTES
+//write out routes (post, out, get)
---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,33 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const expressAsyncHandler = require("express-async-handler");
+
+const authMiddleware = asyncHandler(async (req, res, next) => {
+  let token;
+  if (req?.header?.authorization?.startsWith("Bearer")) {
+    token = req.header.authorization.split(" ")[1];
+    try {
+      if (token) {
+        const decorded = jwt.verify(token, process.env.JWT_SECRET);
+        const User = await User.findById(decorded?.id);
+        req.user = User;
+        next();
+      }
+    } catch (error) {
+      throw new Error("Not Authorized token expired, Login again");
+    }
+  } else {
+    throw new Error("There is no token attached to header");
+  }
+});
+const isAdmin = asyncHandler(async (req, res, next) => {
+  const { email } = req.user;
+  const adminUser = await User.findOne({ email });
+  if (adminUser.role !== "admin") {
+    throw new Error("You are not an admin");
+  } else {
+    next();
+  }
+});
+module.exports = { authMiddleware, isAdmin };
