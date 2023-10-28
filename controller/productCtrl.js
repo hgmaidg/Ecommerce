5a14082f02cfe1d06fed0d4ef080cc5139c6dc7a
Hoàng Mai <hgmaidg@gmail.com>
2023-10-28 13:47:12 +0700

    Filtering/Sorting Product

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const Product = require("../models/productModel");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
@@ -51,8 +52,43 @@ const getaProduct = asyncHandler(async (req, res) => {
 
 const getallProduct = asyncHandler(async (req, res) => {
   try {
-    const getallProduct = await Product.find(req.query);
-    res.json(getallProduct);
+    //Filtering
+    const queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    console.log(queryObj);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Product.find(JSON.parse(queryStr));
+
+    //Sorting
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    //Limiting the fields
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    //Pagination
+
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    console.log(page, limit, skip);
+
+    const product = await query;
+    res.json(product);
   } catch (error) {
     throw new Error(error);
   }
