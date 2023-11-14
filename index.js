364da729fa0e22c76e2ecb064fc3b15a75b77d61
Hoàng Mai <hgmaidg@gmail.com>
2023-11-14 13:58:32 +0700

    Brand
    Create, Update, Delete Brands

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
 const categoryRouter = require("./routes/categoryRoute");
 const blogCatRouter = require("./routes/blogCatRoute");
+const brandRouter = require("./routes/brandRoute");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 dbConnect();
@@ -24,6 +25,7 @@ app.use("/api/product", productRouter);
 app.use("/api/blog", blogRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/blogcategory", blogCatRouter);
+app.use("/api/brand", brandRouter);
 
 app.use(notFound);
 app.use(errorHandler);
