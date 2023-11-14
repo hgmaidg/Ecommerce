36194e2474068393fc540a5b77ed2219a67ffd5d
Hoàng Mai <hgmaidg@gmail.com>
2023-11-14 13:30:05 +0700

    Blog Category
    Create, Delete, Update Blog Category

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
 const categoryRouter = require("./routes/categoryRoute");
+const blogCatRouter = require("./routes/blogCatRoute");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 dbConnect();
@@ -22,6 +23,7 @@ app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
 app.use("/api/blog", blogRouter);
 app.use("/api/category", categoryRouter);
+app.use("/api/blogcategory", blogCatRouter);
 
 app.use(notFound);
 app.use(errorHandler);
